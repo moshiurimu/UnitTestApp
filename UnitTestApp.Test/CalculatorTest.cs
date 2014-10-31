8a429029a7f71e071646ccfe2ef48c4a250c6eb3
Moshiur Rahman <moshiur.imu@gmail.com>
2014-10-31 09:40:07 +0600

    Unit Test App

---
+++ UnitTestApp.Test/CalculatorTest.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace UnitTestApp.Test
+{
+    class CalculatorTest
+    {
+        private Calculator aCalculator= new Calculator();
+
+
+
+
+        [Test]
+        public void AddTest_Check_If_Can_Add_Two_Positive_Numbers()
+        {
+            Assert.AreEqual(10, aCalculator.Add(5, 5));
+        }
+
+
+        [Test]
+        public void AddTest_Check_If_Can_Add_Two_Negative_Numbers()
+        {
+
+            Assert.AreEqual(-10, aCalculator.Add(-5, -5));
+        }
+        
+        
+        [Test]
+        public void AddTest_Check_If_Can_Add_Two_Double_Positive_Numbers()
+        {
+            Assert.AreEqual(5,aCalculator.Add(2.5,2.5));
+        }
+
+
+        [Test]
+         public void AddTest_Check_If_Can_Add_Two_Double_Negative_Numbers()
+        {
+            
+            Assert.AreEqual(-11,aCalculator.Add(-5.5,-5.5));
+        }
+    }
+}
---
+++ UnitTestApp/Calculator.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitTestApp
+{
+    public class Calculator
+    {
+        public double Add( double fristNum, double secondNum)
+        {
+            return fristNum + secondNum;
+        }
+    }
+}
